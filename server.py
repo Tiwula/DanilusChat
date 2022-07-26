e698ba73a6e063e43f480a3c09243b29a7eda617
FelixFromDiscord <sidorovf996@gmail.com>
2022-07-27 01:01:56 +0300

    plugin api v2

--- server.py
+++ server.py
@@ -10,27 +10,10 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import serialization
 import sys, traceback, importlib, os, requests
+import sfuncs as sf
 
 
 con = Console()
-
-try:
-    response = requests.get("https://raw.githubusercontent.com/Danilus-s/DanilusChat/main/sfuncs.py")
-    try:
-        with open("sfuncs.py", "r") as f:
-            data = f.read()
-            if len(data) != len(response.text):
-                get = con.input(f'[cyan bold][XX-XX-XXXX XX:XX:XX][/] [yellow bold]<sfuncs>[/] "sfuncs" updated. Your filesize: {len(data)}; New filesize: {len(response.text)}. Update? [Y/n]')
-                if get.lower().replace(' ', '').startswith('y') or get.replace(' ', '') == '':
-                    with open('sfuncs.py', 'w') as f:
-                        f.write(response.text)
-    except:
-        with open('sfuncs.py', 'w') as f:
-            f.write(response.text)
-except:
-    pass
-
-import sfuncs as sf
 
 
 settings = {}
@@ -68,88 +51,86 @@ try:
 except:
     pass
 
-plug = []
-pluginfo = {}
-debugPlugInfo = {}
-for path in os.listdir('plugins'):
-    try:
-        if path[len(path)-3:] == '.py':
-            pname = path[0:len(path)-3]
-            plug.append(importlib.import_module('plugins.'+pname))
-            sf.log("plugins", f"{pname} initialized.")
+plugmodules = {}
+pluglist = []
+plfiles = [f for f in os.listdir('plugins') if os.path.isfile(os.path.join('plugins', f))]
+for i in plfiles:
+    if i[len(i)-3:] == ".py":
+        try:
+            plugname = i[:-3]
+            plugmodules[i] = importlib.import_module('plugins.' + plugname)
             try:
-                pluginfo[pname] = plug.plugininfo
-            except:
-                pluginfo[pname] = {"name": pname, "author": "NoName", "version": "1.0"}
-            debugPlugInfo[plug[len(plug)-1]] = pname
-    except Exception as ex:
-        sf.log('debug', 'Error loading plugin '+pname)
-        sf.log('debug', ex)
-sf.pluginfo = pluginfo
+                plugdata = plugmodules[i].plugin.get_data()
+                pluglist.append(plugdata)
+            except Exception as exc:
+                sf.log("plugins", "Error while trying to load plugin data of " + i)
+                sf.log("plugins", exc)
+                pass
+        except Exception as exc:
+            sf.log("plugins", "Error while trying to import plugin " + i)
+            sf.log("plugins", exc)
+            pass
 
-for i in plug:
+
+for i in plugmodules:
     try:
-        i.init()
+        plugmodules[i].plugin.events["init"]()
     except AttributeError:
         pass
     except Exception as ex:
-        sf.log('debug', 'Error loading plugin '+debugPlugInfo[i])
+        sf.log('debug', 'Error loading plugin ' + i)
         sf.log('debug', ex)
-        with open('logs\\'+debugPlugInfo[i]+'.log', 'w') as f:
-                    traceback.print_exc(file=f)
 
 
 def sendToAll(data, event="receive message") -> None:
     if event == "receive message":
-        for i in plug:
+        for i in plugmodules:
             try:
-                i.receive(data)
+                plugmodules[i].plugin.events["receive"](data)
             except AttributeError:
                 pass
             except Exception as ex:
-                sf.log('debug', 'Error execute plugin callback '+debugPlugInfo[i])
+                sf.log('debug', 'Error execute plugin callback')
                 sf.log('debug', ex)
-                exc_type, exc_value, exc_traceback = sys.exc_info()
-                with open('logs\\'+debugPlugInfo[i]+'.log', 'w') as f:
-                    traceback.print_exc(file=f)
     elif event == "new user":
-        for i in plug:
+        for i in plugmodules:
             try:
-                i.newUser(data)
+                plugmodules[i].plugin.events["newUser"](data)
             except AttributeError:
                 pass
             except Exception as ex:
-                sf.log('debug', 'Error execute plugin callback '+debugPlugInfo[i])
+                sf.log('debug', 'Error execute plugin callback ' + i)
                 sf.log('debug', ex)
-                with open('logs\\'+debugPlugInfo[i]+'.log', 'w') as f:
-                    traceback.print_exc(file=f)
-            
 
-def encrypt(pubKey ,message) -> str:
+
+def encrypt(pubKey, message) -> str:
     message = str(message).encode('utf-8')
-    return  pubKey.encrypt(message, padding.OAEP
-                    (mgf=padding.MGF1( algorithm=hashes.SHA256() ), algorithm=hashes.SHA256(), label=None)
-                    )
+    return pubKey.encrypt(message, padding.OAEP
+    (mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None)
+                          )
+
+
 def decrypt(privKey, message) -> bytes:
-    return  privKey.decrypt(message, padding.OAEP(
-                    mgf=padding.MGF1( algorithm=hashes.SHA256() ), algorithm=hashes.SHA256(), label=None)
-                    ).decode('utf-8')
+    return privKey.decrypt(message, padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None)
+                           ).decode('utf-8')
 
 
 def dump(prefix='def'):
-    d = dt.now() 
+    d = dt.now()
     dumpName = d.strftime('logs\\dump_' + prefix + '_%d-%m-%Y_%H.%M.%S.log')
     dat = globals()
     with open(dumpName, 'w') as f:
         f.write('--------MAIN--------\n\n')
         for i in dat:
-            f.write(str(i) + "\t\t = \t" + str(dat[i]) + "\n") 
+            f.write(str(i) + "\t\t = \t" + str(dat[i]) + "\n")
         f.write('--------SF--------\n\n')
         sf.dump(f)
 
+
 def handle(client):
     while True:
-        #saveData(client)
+        # saveData(client)
         sf.canSend = True
         try:
             message = decrypt(sf.clients[client].privKey, client.recv(2048)).strip()
@@ -162,7 +143,13 @@ def handle(client):
             if sf.canSend:
                 if message[0:1] == '/':
                     com = str(message[1:]).split(' ')
-                    
+
+                    for pl in plfiles:
+                        for plcmd in plugmodules[pl].plugin.get_commands():
+                            if com[0] == plcmd["name"]:
+                                args = [i for i in com[1:] if len(com) > 1]
+                                plcmd["callback"](args, client)
+
                     if com[0] == 'admin' and not sf.clients[client].admin and len(com) == 2:
                         sf.log('debug', com[1] + ' ' + settings['Password'])
                         if com[1] == settings['Password']:
@@ -183,7 +170,7 @@ def handle(client):
                                 break
                         if ok == False:
                             client.send(encrypt(sf.clients[client].pubKey, "<server -> me> Unsuccessfully"))
-                            
+
                     elif com[0] == 'unadmin' and sf.clients[client].admin and len(com) == 2:
                         ok = False
                         for cl in sf.clients:
@@ -202,7 +189,8 @@ def handle(client):
                                 oldNick = sf.clients[cl].displayNick
                                 sf.clients[cl].displayNick = com[2]
                                 sf.log('debug', f'send "nick" to {sf.clients[client].displayNick}')
-                                client.send(encrypt(sf.clients[client].pubKey, f"<server -> me> nickname of {oldNick} set to {com[2]}."))
+                                client.send(encrypt(sf.clients[client].pubKey,
+                                                    f"<server -> me> nickname of {oldNick} set to {com[2]}."))
                                 sf.broadcast(f"{oldNick} is now {com[2]}.")
                                 sf.saveData(cl)
                                 dump('nick')
@@ -213,22 +201,24 @@ def handle(client):
                             if str(sf.clients[cl].id) == com[1] and sf.clients[cl].addr != settings['IPhost']:
                                 nick = sf.clients[cl].displayNick
                                 nick2 = sf.clients[client].displayNick
-                                sf.log('debug','Found ' + sf.clients[cl].displayNick)
-                                cl.send(encrypt(sf.clients[cl].pubKey, f"<server -> me> You were kicked by {sf.clients[client].displayNick}."))
+                                sf.log('debug', 'Found ' + sf.clients[cl].displayNick)
+                                cl.send(encrypt(sf.clients[cl].pubKey,
+                                                f"<server -> me> You were kicked by {sf.clients[client].displayNick}."))
                                 cl.close()
                                 sf.clients.pop(cl)
                                 sf.broadcast(f"{nick} kicked by {nick2}.")
                                 dump('kick')
                                 break
 
-                    elif com[0] == 'ban' and sf.clients[client].admin and len(com) == 2:                       
+                    elif com[0] == 'ban' and sf.clients[client].admin and len(com) == 2:
                         for cl in sf.clients:
                             if sf.clients[cl].nickname == com[1] and sf.clients[cl].addr != settings['IPhost']:
                                 nick = sf.clients[cl].displayNick
                                 nick2 = sf.clients[client].displayNick
-                                sf.log('debug','Found ' + sf.clients[cl].displayNick)
+                                sf.log('debug', 'Found ' + sf.clients[cl].displayNick)
                                 sf.banNick(sf.clients[cl].nickname)
-                                cl.send(encrypt(sf.clients[cl].pubKey, f"<server -> me> You were banned by {sf.clients[client].displayNick}."))
+                                cl.send(encrypt(sf.clients[cl].pubKey,
+                                                f"<server -> me> You were banned by {sf.clients[client].displayNick}."))
                                 cl.close()
                                 sf.clients.pop(cl)
                                 sf.broadcast(f"{nick} banned by {nick2}.")
@@ -242,8 +232,9 @@ def handle(client):
                             if sf.clients[cl].addr == com[1]:
                                 nick = sf.clients[cl].displayNick
                                 nick2 = sf.clients[client].displayNick
-                                sf.log('debug','Found ' + sf.clients[cl].displayNick)
-                                cl.send(encrypt(sf.clients[cl].pubKey, f"<server -> me> You were banned by {sf.clients[client].displayNick}."))
+                                sf.log('debug', 'Found ' + sf.clients[cl].displayNick)
+                                cl.send(encrypt(sf.clients[cl].pubKey,
+                                                f"<server -> me> You were banned by {sf.clients[client].displayNick}."))
                                 cl.close()
                                 sf.clients.pop(cl)
                                 sf.broadcast(f"{nick} banned by {nick2}.")
@@ -265,8 +256,10 @@ def handle(client):
                         sf.log('debug', f'dm from {sf.clients[client].displayNick} to {com[1]}: {sf.listJoin(com[2:])}')
                         for cl in sf.clients:
                             if sf.clients[cl].displayNick == com[1]:
-                                client.send(encrypt(sf.clients[client].pubKey, f"<me -> {com[1]}> {sf.listJoin(com[2:])}"))
-                                cl.send(encrypt(sf.clients[cl].pubKey, f"<{sf.clients[client].displayNick} -> me> {sf.listJoin(com[2:])}"))
+                                client.send(
+                                    encrypt(sf.clients[client].pubKey, f"<me -> {com[1]}> {sf.listJoin(com[2:])}"))
+                                cl.send(encrypt(sf.clients[cl].pubKey,
+                                                f"<{sf.clients[client].displayNick} -> me> {sf.listJoin(com[2:])}"))
                                 break
 
                     elif com[0] == 'me' and len(com) >= 2:
@@ -275,11 +268,13 @@ def handle(client):
 
                     elif com[0] == 'try' and len(com) >= 2:
                         sf.log('debug', f'try from {sf.clients[client].displayNick}: {sf.listJoin(com[1:])}')
-                        tr = bool(random.randint(0,1))
+                        tr = bool(random.randint(0, 1))
                         if tr:
-                            sf.broadcast(f"*{sf.clients[client].displayNick} {sf.removeFormat(sf.listJoin(com[1:]))} &2(Successfully)&r")
+                            sf.broadcast(
+                                f"*{sf.clients[client].displayNick} {sf.removeFormat(sf.listJoin(com[1:]))} &2(Successfully)&r")
                         else:
-                            sf.broadcast(f"*{sf.clients[client].displayNick} {sf.removeFormat(sf.listJoin(com[1:]))} &4(Unsuccessfully)&r")
+                            sf.broadcast(
+                                f"*{sf.clients[client].displayNick} {sf.removeFormat(sf.listJoin(com[1:]))} &4(Unsuccessfully)&r")
 
                     elif com[0] == 'do' and len(com) >= 2:
                         sf.log('debug', f'do from {sf.clients[client].displayNick}: {sf.listJoin(com[1:])}')
@@ -313,12 +308,13 @@ def handle(client):
                         else:
                             sf.clients[client].color = col
                         sf.saveData(client)
-                    
+
                     elif com[0] == 'prefix' and sf.clients[client].admin and len(com) >= 3:
                         for cl in sf.clients:
                             if str(sf.clients[cl].id) == com[1]:
                                 sf.clients[cl].prefix = sf.removeFont(sf.listJoin(com[2:]))
-                                if sf.removeFont(sf.listJoin(com[2:])) == '' or sf.removeFont(sf.listJoin(com[2:])) == 'remove':
+                                if sf.removeFont(sf.listJoin(com[2:])) == '' or sf.removeFont(
+                                        sf.listJoin(com[2:])) == 'remove':
                                     sf.clients[cl].prefix = None
                                 sf.saveData(cl)
                                 break
@@ -328,7 +324,7 @@ def handle(client):
                         if sf.removeFont(sf.listJoin(com[1:])) == '' or sf.removeFont(sf.listJoin(com[1:])) == 'remove':
                             sf.clients[client].prefix = None
                         sf.saveData(client)
-                    
+
                     elif com[0] == 'dump' and sf.clients[client].admin:
                         dump('command by ' + sf.clients[client].displayName)
 
@@ -380,10 +376,12 @@ def handle(client):
             sf.log("debug", ex)
             break
 
+
 def stop():
     sf.log("server", "server stoped.")
     server.close()
     exit(0)
+
 
 def receive():
     while True:
@@ -391,20 +389,18 @@ def receive():
             client, addr = server.accept()
             getReady = client.recv(2048).decode('utf-8')
             if getReady == 'COM:READY':
-                
-                
-                #gen key pairs
+
+                # gen key pairs
                 privKey = rsa.generate_private_key(public_exponent=65537, key_size=2048, backend=default_backend())
-                
-                
-                #send server pub key
+
+                # send server pub key
                 client.send('COM:SERVERPUB'.encode('utf-8'))
                 getReady = client.recv(2048).decode('utf-8')
                 sf.log("debug", f"{addr[0]}: send server pub key...")
                 if getReady == 'COM:READYSERVERPUB':
                     pub = privKey.public_key()
                     pem = pub.public_bytes(encoding=serialization.Encoding.PEM,
-                                    format=serialization.PublicFormat.SubjectPublicKeyInfo)
+                                           format=serialization.PublicFormat.SubjectPublicKeyInfo)
                     client.send(pem)
                     sf.log("debug", f"{addr[0]}: send server pub key --> [green]+[/]")
                 else:
@@ -412,27 +408,23 @@ def receive():
                     sf.log("debug", f"{addr[0]}: send server pub key --> [red]-[/]")
                     continue
 
-
-                #get client pub key
+                # get client pub key
                 getReady = client.recv(2048).decode('utf-8')
                 sf.log("debug", f"{addr[0]}: get client pub key...")
                 if getReady == 'COM:CLIENTPUB':
                     client.send('COM:READYCLIENTPUB'.encode('utf-8'))
                     pubKey = client.recv(2048)
-                    pubKey = serialization.load_pem_public_key( pubKey, backend=default_backend() )
+                    pubKey = serialization.load_pem_public_key(pubKey, backend=default_backend())
                     sf.log("debug", f"{addr[0]}: get client pub key --> [green]+[/]")
                 else:
                     client.close()
                     sf.log("debug", f"{addr[0]}: get client pub key --> [red]-[/]")
                     continue
 
-
-                #nick
+                # nick
                 client.send("COM:NICK".encode('utf-8'))
                 nickname = client.recv(2048).decode('utf-8')
-                
 
-                
                 nick = sf.prepareNick(nickname)
                 if not settings['FormatingNick']:
                     nick = sf.removeFormat(nick)
@@ -444,15 +436,16 @@ def receive():
 
                 sf.addUser(client, nick, addr[0], pubKey, privKey)
                 sendToAll((client, nick, addr[0]), "new user")
-                
+
                 sf.loadData(client)
                 sf.saveData(client)
 
-                sf.log("server", f"Connected with: {str(addr[0])} and with nickname: [{sf.clients[client].id}] {sf.clients[client].displayNick}({sf.clients[client].nickname}).")
+                sf.log("server",
+                       f"Connected with: {str(addr[0])} and with nickname: [{sf.clients[client].id}] {sf.clients[client].displayNick}({sf.clients[client].nickname}).")
 
                 thread = th.Thread(target=handle, args=(client,))
                 thread.start()
-                
+
                 sf.broadcast(f"{sf.clients[client].displayNick} connected! Have fun :)")
             else:
                 client.close()
@@ -460,11 +453,9 @@ def receive():
             ex_type, ex, tb = sys.exc_info()
             traceback.print_tb(tb)
             print(str(ex))
-        
-
 
 
 sf.log("server", "server started.")
-sf.log("server",  f"work at {settings['IPhost']}@{settings['PortHost']}.")
+sf.log("server", f"work at {settings['IPhost']}@{settings['PortHost']}.")
 
 receive()
--- sfuncs.py
+++ sfuncs.py
@@ -1,8 +1,11 @@
+import functools
 from datetime import datetime as dt
 from rich.console import Console
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
-import os, json
+import os
+import json
+import typing
 
 con = Console()
 
@@ -12,8 +15,6 @@ def getID():
         if i[len(i)-5:] == ".json":
             count += 1
     return count
-
-
 
 class User:
     def __init__(self, nickname, addr, pubKey, privKey, admin=False, prefix=None, color='&r', id=None):
@@ -29,7 +30,42 @@ class User:
         self.prefix = prefix
         self.color = color
         self.displayNick = nickname
-        
+
+
+class PluginError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class Plugin:
+    def __init__(self, name: str, author: str, version: str):
+        self.name = name
+        self.author = author
+        self.version = version
+        self.commands = []
+        self.events = {}
+
+    def get_data(self):
+        return {"name": self.name, "author": self.author, "version": self.version}
+
+    def get_commands(self):
+        return self.commands
+
+    def command(self, name: str, description: str):
+        if " " in name:
+            raise PluginError('Can\'t use spaces in command name')
+
+        def command_decorator(func):
+            log(self.name, "Added command " + name)
+            self.commands.append({"name": name, "description": description, "callback": func})
+        return command_decorator
+
+    def set_callback(self, func: typing.Callable, cb: str):
+        cblist = ['init', 'newUser', 'receive']
+        if cb not in cblist:
+            raise PluginError('Unknown callback! Correct callbacks: ' + cblist)
+        self.events[cb] = func
+
 
 clients = {}
 
@@ -41,73 +77,70 @@ def getClients() -> clients:
 
 settings = {}
 
-d = dt.now() 
+d = dt.now()
 try:
     os.mkdir('logs')
 except:
     pass
 logname = d.strftime('logs\\server_%d-%m-%Y_%H.%M.%S.log')
 
-canSend = True
-
 def log(fro, message) -> None:
-    try:
+    if fro != 'debug' or fro == 'debug' and settings['Debug']:
         d = dt.now()
         newd = d.strftime('[%d-%m-%Y %H:%M:%S]')
         f = open(logname, 'a')
         f.write(f'{newd} <{fro}> {message}' + '\n')
+        con.print(f'[cyan bold]{newd}[/] [yellow bold]<{fro}>[/] {message}')
         f.close()
-        if fro != 'debug' or fro == 'debug' and settings['Debug']:
-            con.print(f'[cyan bold]{newd}[/] [yellow bold]<{fro}>[/] {message}')
-        
-    except:
-        d = dt.now()
-        newd = d.strftime('[%d-%m-%Y %H:%M:%S]')
-        f = open(logname, 'a')
-        f.write(f'{newd} <{fro}> {message}' + '\n')
-        f.close()
-        if fro != 'debug' or fro == 'debug' and settings['Debug']:
-            con.print(f'[cyan bold]{newd}[/] [yellow bold]<!>[/] {str(message)}')
-        
 
-def encrypt(pubKey ,message) -> str:
+
+def encrypt(pubKey, message) -> str:
     message = str(message).encode('utf-8')
-    return  pubKey.encrypt(message, padding.OAEP
-                    (mgf=padding.MGF1( algorithm=hashes.SHA256() ), algorithm=hashes.SHA256(), label=None)
-                    )
+    return pubKey.encrypt(message, padding.OAEP
+    (mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None)
+                          )
+
+
 def decrypt(privKey, message) -> bytes:
-    return  privKey.decrypt(message, padding.OAEP(
-                    mgf=padding.MGF1( algorithm=hashes.SHA256() ), algorithm=hashes.SHA256(), label=None)
-                    ).decode('utf-8')
+    return privKey.decrypt(message, padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None)
+                           ).decode('utf-8')
+
 
 def broadcast(message) -> None:
     log('debug', f'broadcast started > {message}')
     for client in clients:
         client.send(encrypt(clients[client].pubKey, message))
 
+
 def listJoin(org_list, seperator=' ') -> str:
     return seperator.join(org_list)
 
+
 def removeFormat(text) -> str:
     txt = str(text)
-    flist = [ '&0', '&1', '&2', '&3', '&4', '&5', '&6', '&7', '&8', '&9', '&a', '&b', '&c', '&d', '&e', '&f', '&n', '&m', '&l', '&o', '&r' ]
+    flist = ['&0', '&1', '&2', '&3', '&4', '&5', '&6', '&7', '&8', '&9', '&a', '&b', '&c', '&d', '&e', '&f', '&n', '&m',
+             '&l', '&o', '&r']
     for i in flist:
         txt = txt.replace(i, '')
     return txt
+
 
 def prepareNick(text) -> str:
     txt = str(text)
-    flist = [ ' ', '/', '\\', ':', '*', '?', '"', '<', '>', '|' ]
+    flist = [' ', '/', '\\', ':', '*', '?', '"', '<', '>', '|']
     for i in flist:
         txt = txt.replace(i, '')
     return txt
 
+
 def removeFont(text) -> str:
     txt = str(text)
-    flist = [ '&n', '&m', '&l', '&o' ]
+    flist = ['&n', '&m', '&l', '&o']
     for i in flist:
         txt = txt.replace(i, '')
     return txt
+
 
 def banNick(nick) -> None:
     text = ""
@@ -123,6 +156,7 @@ def banNick(nick) -> None:
         f.write(nick + '\n')
         f.close()
 
+
 def banIP(ip) -> None:
     text = ""
     try:
@@ -136,6 +170,7 @@ def banIP(ip) -> None:
         f = open('banip-list.txt', 'a')
         f.write(ip + '\n')
         f.close()
+
 
 def unbanNick(nick) -> None:
     text = ""
@@ -153,6 +188,7 @@ def unbanNick(nick) -> None:
         f.write(textToWrite)
         f.close()
 
+
 def unbanIP(ip) -> None:
     text = ""
     try:
@@ -168,7 +204,8 @@ def unbanIP(ip) -> None:
         f = open('banip-list.txt', 'w')
         f.write(textToWrite)
         f.close()
-    
+
+
 def getBan(nick, addr) -> bool:
     text = []
     ret = False
@@ -189,7 +226,7 @@ def getBan(nick, addr) -> bool:
         ret = True
 
     return ret
-    
+
 
 def loadData(client):
     back = clients[client]
@@ -206,6 +243,7 @@ def loadData(client):
         log('server', f"Error loading data for user {clients[client].displayNick}.")
         log('server', ex)
 
+
 def saveData(client):
     cli = {}
     cli['id'] = clients[client].id
@@ -218,10 +256,12 @@ def saveData(client):
     with open(f'userdata\\{clients[client].addr} {clients[client].nickname}.json', 'w') as f:
         json.dump(cli, f)
 
+
 def dump(f):
     dat = globals()
     for i in dat:
-        f.write(str(i) + "\t\t = \t" + str(dat[i]) + "\n") 
+        f.write(str(i) + "\t\t = \t" + str(dat[i]) + "\n")
+
 
 def send(client, message):
     client.send(encrypt(clients[client].pubKey, message))
